a6ea8aa1f7660a4e72a0830b3b226511d7a7c880
Asel Mukanova <aseltk0@gmail.com>
2026-05-02 13:43:26 +0500

    Add database connection script in NJFKJDVKJDFN.php

---
+++ PHP/NJFKJDVKJDFN.php
@@ -0,0 +1,19 @@
+<?php
+
+try {
+    $host = 'localhost';
+    $db = 'daneqer';
+    $user = 'root';
+    $pass = '';
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo 'Ошибка подключения к БД: '.$e->getMessage();
+    exit;
+}
